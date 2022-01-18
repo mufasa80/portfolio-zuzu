f95a270452c810a62dbf8d20eb806c6cc7b57c49
mufasa80 <86437411+mufasa80@users.noreply.github.com>
2022-01-18 01:58:02 +0100

    zuzu af portfilio

--- Templates/HTML/PHP/zuzu.php
+++ Templates/HTML/PHP/zuzu.php
@@ -1,20 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Order Form Example</title>
+    <link href="sushi.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+</head>
 
-<?php
-    require_once('header.php');
+<body>
+    <h1><a href="/Templates/HTML/PHP/index.php">Welkom bij ZUZU</a></h1>
+    <img src="/img/2163026.jpg" alt="lol">
+    <link rel="stylesheet" href="/Public/CSS/CSS/zuzu.css">
+
+    <?php
+    // require "index.php";
     require_once('database.php');
-    
-?>
-<!DOCTYPE html> 
-<html lang='en'> 
+    require('process2.php');
+    // (A) PROCESS ORDER FORM
+    if (isset($_POST["name"])) {
 
-    <img src='/img/2163026.jpg' alt='photo of me' /> 
+        echo $result == ""
+            ? "<div class='notify'>Bedankt! uw bestelling komt eraan!</div>"
+            : "<div class='notify'>$result</div>";
+    }
+    ?>
 
-</html> 
+    <form id="orderform" method="post" target="_self">
+        <label for="name">Naam:</label>
+        <input type="text" name="name" required value="" />
+        
+        <label for="email">Email:</label>
+        <input type="email" name="email" required value="" />
+        <br><br>
+        <label for="address">Address:</label>
+        <input type="text" name="address" required value="" />
+        
+        <label for="zipcode">Postcode:</label>
+        <input type="text" name="zipcode" required value="" />
+        <br><br>
+        <label for="city">Stad:</label>
+        <input type="text" name="city" required value="" />
 
-<link rel="stylesheet" href="/Public/CSS/CSS/zuzu.css">
-<div wrapper>
-    <p style="font-family: Candara">Wat wilt u bestellen?</p>
-    <div class="order">
+
+
+
         <form method="post">
             <select name="sushi_id">
                 <option selected disabled value="">Kies een sushi!</option>
@@ -24,110 +52,87 @@
                 $sql->execute();
                 $result = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-                foreach($result as $data) {
+                foreach ($result as $data) {
                     echo "
-                        <option selected enabled value='$data[name]'>" . $data['name'] . "</option>
-                    ";
+                    <option selected enabled id='$data[id]' value='$data[name]'>" . $data['name'] .  ". Vooraad: " . $data['amount'] . "</option>
+                ";
                 }
                 ?>
             </select> <br>
-            <p style="font-family: Candara">Hoeveelheid:</p>
-            <input type="number" name="amount" value="1"> <br>
-            <p style="font-family: Candara">First Name:</p>
-            <input type="text" name="fname">
-            <p style="font-family: Candara">Last Name:</p>
-            <input type="text" name="lname">
-            <p style="font-family: Candara">Address</p>
-            <input type="text" name="address">
-            <p style="font-family: Candara">City</p>
-            <input type="text" name="city">
-            <p style="font-family: Candara">Zipcode</p>
-            <input type="text" name="zipcode"> <br> <br>
+            <p class=hoeveelheid>Aantal</p>
+            <input type="number" name="amount" placeholder="1"> <br>
+
             <input type="submit" name="submit">
         </form>
-    </div>
-</div>
 
-<?php
-    global $pdo;
-    if(isset($_POST['submit'])) {
-        if ($_POST['amount'] != 0 && !empty($_POST['amount']) && !empty($_POST['fname']) && !empty($_POST['lname']) && !empty($_POST['address']) && !empty($_POST['city']) && !empty($_POST['zipcode'])) {
-            $amount = $_POST['amount'];
-            $sushi = $_POST['sushi_id'];
-            $fname = $_POST['fname'];
-            $lname = $_POST['lname'];
-            $address = $_POST['address'];
-            $city = $_POST['city'];
-            $zipcode = $_POST['zipcode'];
+        <?php
+        global $pdo;
+        if (isset($_POST['submit'])) {
+            if ($_POST['amount'] != 0 && !empty($_POST['amount'])) {
+                $amount = $_POST['amount'];
+                $sushi = $_POST['sushi_id'];
 
-            $sql = $pdo->prepare("INSERT INTO customer (fname, lname, address, city, zipcode) VALUES ('$fname', '$lname', '$address', '$city', '$zipcode')");
-            $sql->execute();
 
-            $sql = $pdo->prepare("SELECT * FROM customer");
-            $sql->execute();
-            $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+                $sql = $pdo->prepare("SELECT * FROM sushi");
+                $sql->execute();
+                $result = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-            foreach ($result as $data) {
-                if ($data['address'] == $address) {
-                    $fn = $data['fname'];
-                    $ln = $data['lname'];
-                    $ad = $data['address'];
-                    $ci = $data['city'];
-                    $zi = $data['zipcode'];
-                }
-            }
-
-            $sql = $pdo->prepare("SELECT * FROM sushi");
-            $sql->execute();
-            $result = $sql->fetchAll(PDO::FETCH_ASSOC);
-
-            foreach ($result as $data) {
-                if ($data['name'] == $sushi) {
-                    $sn = $data['name'];
-                    $sp = $data['price'];
-                    $id = $data['id'];
-                    $sa = $amount;
-                    if ($sa > 1) {
-                        $stp = $data['price'] * $sa;
-                    } else {
-                        $stp = $sp;
+                foreach ($result as $data) {
+                    if ($data['name'] == $sushi) {
+                        $sn = $data['name'];
+                        $sp = $data['price'];
+                        $id = $data['id'];
+                        $sa = $amount;
+                        if ($sa > 1) {
+                            $stp = $data['price'] * $sa;
+                        } else {
+                            $stp = $sp;
+                        }
                     }
                 }
+
+                $new_amount = $data['amount'] - $amount;
+
+                if ($amount > $data['amount']) {
+                    echo 'Je kunt niet meer bestellen dan onze vooraad.';
+                } else {
+                    $sql2 = $pdo->prepare("UPDATE sushi SET amount = $new_amount WHERE id = $id");
+                    $sql2->execute();
+
+                    echo "<table>
+
+                    <tr>
+                        <th>Sushi</th>
+                        <th>Prijs</th>
+                        <th>Hoeveelheid</th>
+                        <th>Totaal Prijs</th>
+                    </tr>
+                    <tr>
+                        <td>" . $sn . "</td>
+                        <td>" . $sp . "</td>
+                        <td>" . $sa . "</td>
+                        <td>" . $stp . "</td>
+                    </tr>
+                </table>";
+                }
+
+                // $sql2 = $pdo->prepare("SELECT * FROM sushi WHERE id = '$id'");
+                // $sql2->execute();
+                // $result2 = $sql2->fetchAll(PDO::FETCH_ASSOC);
+
+                // $available = $result2['amount'];
+                // $uitslag = $available - $sa;
+
+                // $sql3 = $pdo->prepare("UPDATE sushi SET amount = '$uitslag' WHERE id = '$id'");
+                // $sql3->execute();
+            } else {
+                echo '<script>alert("Fill in all fields.");</script>';
             }
-
-            echo "
-            <table>
-                <tr>
-                    <th>Voornaam</th>
-                    <th>Achternaam</th>
-                    <th>Adres</th>
-                    <th>Stad</th>
-                    <th>Postcode</th>
-                </tr>
-                <tr>
-                    <td>" . $fn . "</td>
-                    <td>" . $ln . "</td>
-                    <td>" . $ad . "</td>
-                    <td>" . $ci . "</td>
-                    <td>" . $zi . "</td>
-                </tr>
-                <tr>
-                    <th>Sushi</th>
-                    <th>Prijs</th>
-                    <th>Hoeveelheid</th>
-                    <th>Totaal Prijs</th>
-                </tr>
-                <tr>
-                    <td>" . $sn . "</td>
-                    <td>" . $sp . "</td>
-                    <td>" . $sa . "</td>
-                    <td>" . $stp . "</td>
-                </tr>
-            </table>";
-        } else {
-            echo '<script>alert("Fill in all fields.");</script>';
         }
-    }
-?>
-<?php require_once('footer.php'); ?>
+        ?>
 
+
+
+</body>
+
+</html>
